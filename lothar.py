bb2f5f1ec28462029366e463a65276949fae6392
iván <Doodiemin@gmail.com>
2021-05-17 14:32:34 -0300

    Python 1

---
+++ lothar.py
@@ -0,0 +1,13 @@
+def lothar(n):
+  x = 0
+  while n != 1: 
+    if n%2 == 0:
+      n = n/2
+    else:
+      n = n*3 + 1
+    x = x + 1
+  return(x)
+n = 0
+while n<= 1:
+  n = int(input())
+print( lothar(n) )
